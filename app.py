92af4aa4f4a1ef2d08070a4de74a48ed4ada05b4
isaacponsford <isaacponsford@gmail.com>
2021-12-28 14:48:42 +0000

    log in test

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+
+    if request.method == 'POST':
+        username_content = request.form['username']
+        password_content = request.form['password']
+        print(username_content)
+        print(password_content)
+        return redirect('/')
+    else:
+        return render_template('index.html')
+
+@app.route('/log-in')
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
