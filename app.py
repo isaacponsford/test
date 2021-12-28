9289f43530b4310020bb2a69c365bd773a057553
isaacponsford <isaacponsford@gmail.com>
2021-12-28 18:36:18 +0000

    Functioning Vertical Plane Layout (Fixed 2 Seat Classes)

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from importCSV import planeMetrics
 
 app = Flask(__name__)
+
+planeMetrics = planeMetrics("emb145")
+
+noOfRows = planeMetrics[0]
+noOfColumns = planeMetrics[1]
+capacity = planeMetrics[2]
+capacityArray = planeMetrics[3]
+
+planeLayout = planeMetrics[4]
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    if request.method == 'POST':
-        username_content = request.form['username']
-        password_content = request.form['password']
-        print(username_content)
-        print(password_content)
-        return redirect('/')
-    else:
-        return render_template('index.html')
+    return render_template('index.html', planeLayout = planeLayout)
 
 @app.route('/log-in')
 def login():
---
+++ importCSV.py
@@ -0,0 +1,42 @@
+import csv
+
+def incrementClassCapacity(classNum, capacityArray):
+
+    exists = False
+
+    for i in capacityArray:
+        if i[0] == classNum:
+            exists = True
+            i[1] += 1
+
+    if not exists:
+        capacityArray.append([classNum,1])
+
+def planeMetrics(csv_file):
+
+    csv_file = "C:/Users/Alan/Documents/_Isaac/planeLayouts/" + csv_file + ".csv"
+    capacity = 0
+    noOfRows = 0
+    noOfColumns = 0
+    capacityArray = []
+    planeLayout = []
+
+    with open(csv_file, 'r', encoding='utf-8-sig') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            
+            rowNumber = int(row[0])
+            
+            if rowNumber > noOfRows:
+                noOfRows = rowNumber
+                
+            rowLayout = row[1::]
+            planeLayout.append(rowLayout)
+            noOfColumns = len(rowLayout)
+            
+            for seat in rowLayout:
+                if seat != '':
+                    capacity = capacity + 1
+                    incrementClassCapacity(seat, capacityArray)
+
+    return[noOfRows, noOfColumns, capacity, capacityArray, planeLayout]
