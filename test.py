cbda68a726a660e2156d4f4fa4cc500da68e1372
isaacponsford <isaacponsford@gmail.com>
2022-03-20 20:32:25 +0000

    interpage links

--- test.py
+++ test.py
@@ -2,15 +2,8 @@ from SQLHelper import clearAll, clearPassengersFlightNumber, getAssignedClassTic
 from importCSV import planeMetrics, getPassengerCSV
 from tools import createPassCSVArray, createPassengerCSV, getFullActualArray, getFullClassArray, getPlaneActual, totalCapacity
 import sqlite3
-# happinessFunction("5-1UP1DOWN-S")
 
-# conn, cur = sqlite3.connect()
+# array =[[1,x], [2,x], [3,x], [4,x], [5,x]]
+# createPassCSVArray(array)
 
-# cur.execute("SELECT p.ticketID, a.class, p.class, a.aw, p.preference, p.happiness from passengers as p LEFT JOIN airplaneLayout as a ON a.passengerRef = p.ticketID WHERE p.flightRef = ?", (passRef,))
-# conn.close()
-
-groupHappinessFunction("5-1UP1DOWN-S", 0.1)
-
-#array = [(681, 27, 'A'), (682, 27, 'C'), (680, 27, 'D'), (683, 28, 'A')]
-
-# print(ord('C'))
+#
